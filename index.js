c899df07a64e1074cd69ba4fe53aad22bb489c1b
Greg <caldwell.gregoryj@gmail.com>
2021-03-28 21:31:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const names = ["lisa", "Kaitlin", "Jan"];
+let bDayCard = []
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    //debugger;
+    bDayCard.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+
+  return bDayCard;
+}
+
+writeCards(names); 
